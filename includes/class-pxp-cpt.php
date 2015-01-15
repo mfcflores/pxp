132f64e600ed7f19c9ea4f38311ffb6a251ab514
Albert <albert@illumedia.net>
2015-01-15 15:34:42 +0800

    Added Save Function in Credit Adjustments

--- includes/class-pxp-cpt.php
+++ includes/class-pxp-cpt.php
@@ -54,58 +54,58 @@ class PXP_Cpt
 	public function pxp_register_post_types()
 	{
 		$pxp_cpt = array(
-			'product'		=> 'Product',
-			'order'			=> 'Order',
+			'product'			=> 'Product',
+			'order'				=> 'Order',
 			'transaction'	=> 'Transaction',
 		);
 		
 		foreach($pxp_cpt as $key => $value)
 		{
 			$labels = array(
-				'name'               => _x( $value .'s', 'post type general name', 'excavations' ),
-				'singular_name'      => _x( $value, 'post type singular name' ),
-				'menu_name'          => _x( $value . 's', 'admin menu' ),
-				'name_admin_bar'     => _x( $value, 'add new on admin bar' ),
-				'add_new'            => _x( 'Add New ' . $value, $key ),
-				'add_new_item'       => __( 'Add New ' . $value ),
-				'new_item'           => __( 'New ' . $value ),
-				'edit_item'          => __( 'Edit ' . $value ),
-				'view_item'          => __( 'View ' . $value ),
-				'all_items'          => __( 'All ' . $value ),
-				'search_items'       => __( 'Search ' . $value ),
-				'parent_item_colon'  => __( 'Parent ' . $value . ':' ),
-				'not_found'          => __( 'No ' . $key . ' found.' ),
-				'not_found_in_trash' => __( 'No ' . $key . ' found in Trash.' ),
+				'name'              				=> _x( $value .'s', 'post type general name', 'excavations' ),
+				'singular_name'     			=> _x( $value, 'post type singular name' ),
+				'menu_name'          			=> _x( $value . 's', 'admin menu' ),
+				'name_admin_bar'    		=> _x( $value, 'add new on admin bar' ),
+				'add_new'           			=> _x( 'Add New ' . $value, $key ),
+				'add_new_item'       		=> __( 'Add New ' . $value ),
+				'new_item'           			=> __( 'New ' . $value ),
+				'edit_item'         				=> __( 'Edit ' . $value ),
+				'view_item'         				=> __( 'View ' . $value ),
+				'all_items'         				=> __( 'All ' . $value ),
+				'search_items'      			=> __( 'Search ' . $value ),
+				'parent_item_colon' 		=> __( 'Parent ' . $value . ':' ),
+				'not_found'          			=> __( 'No ' . $key . ' found.' ),
+				'not_found_in_trash' 		=> __( 'No ' . $key . ' found in Trash.' ),
 			);
 
 			$args = array(
-				'labels'             => $labels,
-				'public'             => false,
-				'publicly_queryable' => true,
-				'show_ui'            => true,
-				'show_in_menu'       => true,
-				'query_var'          => true,
-				'rewrite'            => false,
-				'capability_type'    => 'post',
-				'has_archive'        => true,
-				'hierarchical'       => false,
-				'menu_position'      => null,
-				'supports'           => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments' )
+				'labels'             				=> $labels,
+				'public'            					=> false,
+				'publicly_queryable' 		=> true,
+				'show_ui'            			=> true,
+				'show_in_menu'       		=> true,
+				'query_var'          			=> true,
+				'rewrite'           				=> false,
+				'capability_type'    			=> 'post',
+				'has_archive'        			=> true,
+				'hierarchical'      				=> false,
+				'menu_position'      			=> null,
+				'supports'           				=> array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments' )
 			);
 			
 			if( $key == "order" )
 			{
-				$args['supports'] = false;
-				$args['public']				= true;
+				$args['supports'] 			= false;
+				$args['public']					= true;
 				$args['capability_type']	= array( 'pxp_' . $key, 'pxp_' . $key . 's' );
-				$args['map_meta_cap']		= true;
+				$args['map_meta_cap']	= true;
 			}
 			
 			if( $key == "product" )
 			{
-				$args['public']				= true;
+				$args['public']					= true;
 				$args['capability_type']	= array( 'pxp_' . $key, 'pxp_' . $key . 's' );
-				$args['map_meta_cap']		= true;
+				$args['map_meta_cap']	= true;
 			}
 
 			$post_type = 'pxp_' . $key . 's';
@@ -128,20 +128,20 @@ class PXP_Cpt
 		foreach($pxp_cpt as $key => $value)
 		{
 			$labels = array(
-				'name'               => _x( $value .'s', 'post type general name', 'excavations' ),
-				'singular_name'      => _x( $value, 'post type singular name' ),
-				'menu_name'          => _x( $value . 's', 'admin menu' ),
-				'name_admin_bar'     => _x( $value, 'add new on admin bar' ),
-				'add_new'            => _x( 'Add New ' . $value, $key ),
-				'add_new_item'       => __( 'Add New ' . $value ),
-				'new_item'           => __( 'New ' . $value ),
-				'edit_item'          => __( 'Edit ' . $value ),
-				'view_item'          => __( 'View ' . $value ),
-				'all_items'          => __( $value ),
-				'search_items'       => __( 'Search ' . $value ),
-				'parent_item_colon'  => __( 'Parent ' . $value . ':' ),
-				'not_found'          => __( 'No ' . $value . ' found.' ),
-				'not_found_in_trash' => __( 'No ' . $value . ' found in Trash.' ),
+				'name'             				=> _x( $value .'s', 'post type general name', 'excavations' ),
+				'singular_name'     		=> _x( $value, 'post type singular name' ),
+				'menu_name'        		=> _x( $value . 's', 'admin menu' ),
+				'name_admin_bar'    	=> _x( $value, 'add new on admin bar' ),
+				'add_new'            		=> _x( 'Add New ' . $value, $key ),
+				'add_new_item'       	=> __( 'Add New ' . $value ),
+				'new_item'          			=> __( 'New ' . $value ),
+				'edit_item'         			=> __( 'Edit ' . $value ),
+				'view_item'         			=> __( 'View ' . $value ),
+				'all_items'          			=> __( $value ),
+				'search_items'       		=> __( 'Search ' . $value ),
+				'parent_item_colon' 	=> __( 'Parent ' . $value . ':' ),
+				'not_found'          		=> __( 'No ' . $value . ' found.' ),
+				'not_found_in_trash' 	=> __( 'No ' . $value . ' found in Trash.' ),
 			);
 
 			if( $key == "credit_block" )
@@ -150,19 +150,19 @@ class PXP_Cpt
 			}
 			
 			$args = array(
-				'labels'             	=> $labels,
-				'public'             	=> true,
-				'publicly_queryable' 	=> true,
-				'show_ui'            	=> true,
-				'show_in_menu'       	=> true,
-				'query_var'          	=> true,
-				'rewrite'            	=> false,
-				'capability_type'    	=> array( 'pxp_' . $key, 'pxp_' . $key . 's' ),
-				'has_archive'        	=> true,
-				'hierarchical'       	=> false,
-				'menu_position'      	=> null,
-				'supports'           	=> false,
-				'map_meta_cap'			=> true
+				'labels'             				=> $labels,
+				'public'             				=> true,
+				'publicly_queryable' 		=> true,
+				'show_ui'            			=> true,
+				'show_in_menu'       		=> true,
+				'query_var'          			=> true,
+				'rewrite'            				=> false,
+				'capability_type'    			=> array( 'pxp_' . $key, 'pxp_' . $key . 's' ),
+				'has_archive'        			=> true,
+				'hierarchical'       				=> false,
+				'menu_position'      			=> null,
+				'supports'           				=> false,
+				'map_meta_cap'				=> true
 			);
 			
 			if( $key != "credit_block" )
@@ -182,52 +182,52 @@ class PXP_Cpt
 	public function pxp_register_taxonomies()
 	{	
 		$labels = array(
-			'name'                       => _x( 'Product Categories', 'taxonomy general name' ),
-			'singular_name'              => _x( 'Product Category', 'taxonomy singular name' ),
-			'search_items'               => __( 'Search Categories' ),
-			'popular_items'              => __( 'Popular Category' ),
-			'all_items'                  => __( 'All Categories' ),
-			'parent_item'                => null,
-			'parent_item_colon'          => null,
-			'edit_item'                  => __( 'Edit Category' ),
-			'update_item'                => __( 'Update Category' ),
-			'add_new_item'               => __( 'Add New Category' ),
-			'new_item_name'              => __( 'New Category Name' ),
-			'separate_items_with_commas' => __( 'Separate tags with commas' ),
-			'add_or_remove_items'        => __( 'Add or remove category' ),
-			'choose_from_most_used'      => __( 'Choose from the most used tags' ),
-			'not_found'                  => __( 'No category found.' ),
-			'menu_name'                  => __( 'Product Category' ),
+			'name'                      							=> _x( 'Product Categories', 'taxonomy general name' ),
+			'singular_name'             						=> _x( 'Product Category', 'taxonomy singular name' ),
+			'search_items'              						=> __( 'Search Categories' ),
+			'popular_items'              						=> __( 'Popular Category' ),
+			'all_items'                  							=> __( 'All Categories' ),
+			'parent_item'                						=> null,
+			'parent_item_colon'        						=> null,
+			'edit_item'                  							=> __( 'Edit Category' ),
+			'update_item'                						=> __( 'Update Category' ),
+			'add_new_item'               					=> __( 'Add New Category' ),
+			'new_item_name'              					=> __( 'New Category Name' ),
+			'separate_items_with_commas' 		=> __( 'Separate tags with commas' ),
+			'add_or_remove_items'       				=> __( 'Add or remove category' ),
+			'choose_from_most_used'     			=> __( 'Choose from the most used tags' ),
+			'not_found'                							=> __( 'No category found.' ),
+			'menu_name'                 						=> __( 'Product Category' ),
 		);
 
 		$args = array(
-			'hierarchical'          => true,
-			'labels'                => $labels,
-			'show_ui'               => true,
-			'show_admin_column'     => true,
-			'query_var'             => true,
-			'rewrite'               => array( 'slug' => 'product_category' ),
+			'hierarchical'          				=> true,
+			'labels'                					=> $labels,
+			'show_ui'              				=> true,
+			'show_admin_column'     	=> true,
+			'query_var'             				=> true,
+			'rewrite'               				=> array( 'slug' => 'product_category' ),
 		);
 
 		register_taxonomy( 'pxp_product_categories', 'pxp_products', $args );
 		
 		$labels = array(
-			'name'                       => _x( 'Product Tags', 'taxonomy general name' ),
-			'singular_name'              => _x( 'Tag', 'taxonomy singular name' ),
-			'search_items'               => __( 'Search Tags' ),
-			'popular_items'              => __( 'Popular Tag' ),
-			'all_items'                  => __( 'All Tags' ),
-			'parent_item'                => null,
-			'parent_item_colon'          => null,
-			'edit_item'                  => __( 'Edit Tag' ),
-			'update_item'                => __( 'Update Tag' ),
-			'add_new_item'               => __( 'Add New Tag' ),
-			'new_item_name'              => __( 'New Category Name' ),
-			'separate_items_with_commas' => __( 'Separate tags with commas' ),
-			'add_or_remove_items'        => __( 'Add or remove tag' ),
-			'choose_from_most_used'      => __( 'Choose from the most used tags' ),
-			'not_found'                  => __( 'No tag found.' ),
-			'menu_name'                  => __( 'Product Tags' ),
+			'name'                       							=> _x( 'Product Tags', 'taxonomy general name' ),
+			'singular_name'              						=> _x( 'Tag', 'taxonomy singular name' ),
+			'search_items'               						=> __( 'Search Tags' ),
+			'popular_items'              						=> __( 'Popular Tag' ),
+			'all_items'                  							=> __( 'All Tags' ),
+			'parent_item'                						=> null,
+			'parent_item_colon'          					=> null,
+			'edit_item'                  							=> __( 'Edit Tag' ),
+			'update_item'                						=> __( 'Update Tag' ),
+			'add_new_item'               					=> __( 'Add New Tag' ),
+			'new_item_name'              					=> __( 'New Category Name' ),
+			'separate_items_with_commas' 		=> __( 'Separate tags with commas' ),
+			'add_or_remove_items'        				=> __( 'Add or remove tag' ),
+			'choose_from_most_used'      			=> __( 'Choose from the most used tags' ),
+			'not_found'                  						=> __( 'No tag found.' ),
+			'menu_name'                  						=> __( 'Product Tags' ),
 		);
 
 		$args = array(
